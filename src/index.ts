adc8b170336bad3c824785e05125b310974fe605
Dmytro <dmytropostolov@gmail.com>
2020-07-07 16:54:52 +0200

    fix price calculation

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,8 @@ const parseFile = async <T extends { id: string }>(
 const getPrice = (weight: number) => {
   if (weight > 20) return 10
   if (weight > 10) return 5
-  if (weight > 1) return 3
+  if (weight > 5) return 3
+  if (weight > 1) return 2
   return 1
 }
 
